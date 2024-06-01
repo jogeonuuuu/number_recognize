832891e9031098b22963d10072472cdd4bbf9283
jogeonuuuu <wnrdjaos3@gmail.com>
2024-06-01 21:22:16 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -63,28 +63,28 @@ void onMouse(int event, int x, int y, int flags, void* userdata) {
 	case EVENT_LBUTTONDOWN:
 		//line을 그리기위한 pixel value
 		old_pixel = Point(x, y);
-
-		//Clear
-		if (function_area[2].contains(old_pixel))
-			clear_function(src, num_input_area);
-		//Save
-		else if (function_area[0].contains(old_pixel))
-			save_function(src, num_input_area);
-		//Load
-		else if (function_area[1].contains(old_pixel))
-			load_function(src, num_input_area);
-		//Exit
-		else if (function_area[4].contains(old_pixel))
-			ox = false;
-			//jgw::ox = false; //static 방식으로 했을 때
 		break;
-	case EVENT_RBUTTONUP:
+	case EVENT_LBUTTONUP:
 		if (Point(x, y) == old_pixel) {
-
+			//Clear
+			if (function_area[2].contains(old_pixel))
+				clear_function(src, num_input_area);
+			//Save
+			else if (function_area[0].contains(old_pixel))
+				save_function(src, num_input_area);
+			//Load
+			else if (function_area[1].contains(old_pixel))
+				load_function(src, num_input_area);
+			//Exit
+			else if (function_area[4].contains(old_pixel))
+				ox = false;
+				//jgw::ox = false; //static 방식으로 했을 때
 		}
+		break;
 	case EVENT_MOUSEMOVE:
 		if (flags & EVENT_FLAG_LBUTTON) { //왼쪽 마우스가 눌린 상태에서 움직이는지
-			if (num_input_area.contains(Point(x, y)) && num_input_area.contains(old_pixel)) { //500x500 영역 안에 움직이는지
+			//&& num_input_area.contains(old_pixel)
+			if (num_input_area.contains(Point(x, y)) ) { //500x500 영역 안에 움직이는지
 				line(src, old_pixel, Point(x, y), Scalar::all(0), 5);
 				old_pixel = Point(x, y);
 			}
