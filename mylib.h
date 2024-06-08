d50fc496029c8327aca4ab0663c47ec4614c0fd0
jogeonuuuu <wnrdjaos3@gmail.com>
2024-06-09 03:20:22 +0900

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -1,16 +1,22 @@
 #ifndef __MYLIB_H__
 #define __MYLIB_H__
 #define INPUT_WINDOW 500
+#define FUNCT_WINDOW_W 200
+#define FUNCT_WINDOW_H 100
 #define LINE_THICKNESS 2
 #include <opencv2/opencv.hpp>
 #include <iostream>
 using namespace cv;
 using namespace std;
 namespace jgw {
+	//기능 개수에 따른 src 사이즈 조정
+	void src_resize(Mat& src, int array_size);
+
 	//각 기능들 영역 라인 그리기 함수
-	void draw_line(Mat& src, int str_size = 0);
+	void draw_line(Mat& src, int array_size = 0);
+
 	//기능에 대한 문자열 입력창에 삽입
-	void putText_function(Mat& src, String* text);
+	void putText_function(Mat& src, String* text, int array_size);
 
 	//function(각 기능들의 대한 함수)
 	void save_function(Mat& src, Rect& area);
