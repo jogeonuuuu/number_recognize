33f6086484c3eb8c3ad9c3f006877f386665b127
jogeonuuuu <wnrdjaos3@gmail.com>
2024-06-09 01:10:42 +0900

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -7,10 +7,12 @@
 using namespace cv;
 using namespace std;
 namespace jgw {
+	//각 기능들 영역 라인 그리기 함수
+	void draw_line(Mat& src, int str_size = 0);
 	//기능에 대한 문자열 입력창에 삽입
 	void putText_function(Mat& src, String* text);
 
-	//function
+	//function(각 기능들의 대한 함수)
 	void save_function(Mat& src, Rect& area);
 	void load_function(Mat& src, Rect& area);
 	void clear_function(Mat& src, Rect& area);
