7aa0e3d668bcc4cd4a97ae7242f390563156c9c6
jogeonuuuu <wnrdjaos3@gmail.com>
2024-06-11 13:50:23 +0900

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -24,7 +24,7 @@ namespace jgw {
 	void clear_function(Mat& src, Rect& area);
 
 	void feature1(Mat& src, Rect& area);
-	void feature2(Mat& src);
+	void feature2(Mat& src, Rect& area);
 	void run_function(Mat& src, Rect& area);
 }
 #endif // !__MYLIB_H__
